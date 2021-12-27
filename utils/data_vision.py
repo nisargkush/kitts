c0d65514630e191ae718d8d7b0753b06314f1798
Nisarg Kushwaha <kushwahanisarg@gmail.com>
2021-12-27 07:54:19 +0530

    Update data_vision.py

--- utils/data_vision.py
+++ utils/data_vision.py
@@ -1,30 +1,33 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Nov 20 12:52:40 2021
-
-@author: nkushwaha
+Generic data visulaization methods
+Usage:
+    Used inside other project methods. Can also be directly called from notebook    
+Methods:
+    word_graphics : Displays image labels in word cloud format
+    word_distribution : Displays top 50 image labes and it's frequency
+    showimlable : Shows a random image from storage along with it's labels
 """
 
-import io
 import os
-import re
-import argparse
 import pandas as pd
-from pathlib import Path
-from kitts.config import GAPP_CRED, DATA_DIR, ANNOTATED_DIR, RAW_DATA_DIR
+#import argparse
 
 # for visualization
 #%pylab inline
 import random
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from mpl_toolkits.mplot3d import Axes3D
-import seaborn as sns
 plt.style.use('fivethirtyeight')
 
    
 def word_graphics(dataframe):
-    """display image labels in word cloud format"""
+    """
+    Displays image labels in word cloud format
+    Parameters:
+        dataframe: dataframe containing 'img_lables' column to visualize
+    Returns:
+        None
+    """
     from wordcloud import WordCloud
     wordcloud = WordCloud(background_color = 'lightcyan',
                       width = 1200,
@@ -35,7 +38,13 @@ def word_graphics(dataframe):
     plt.title("WordCloud ", fontsize = 10)
     
 def word_distribution(dataframe):  
-    """displays top 50 image labes and it's frequency"""
+    """
+    Displays top 50 image labes and it's frequency
+    Parameters:
+        dataframe: dataframe containing 'img_lables' column to visualize
+    Returns:
+        None
+    """
     from sklearn.feature_extraction.text import CountVectorizer
     import matplotlib.pyplot as plt
     import numpy as np
@@ -53,7 +62,14 @@ def word_distribution(dataframe):
     plt.title("Most Frequently Occuring Words - Top 50")
     
 def showimlable(dataframe, n):
-    """shows a random image from storage along with it's labels"""
+    """
+    Shows a random image from storage along with it's labels
+    Parameters:
+        dataframe: dataframe containing 'img_lables' column to visualize
+        n: Upper limit of random number of images in store
+    Returns:
+        None
+    """
     i = random.randint(0,n)
     data = dataframe.loc[i,['post_date','city','img_lables','img_path']]
     plt.figure(figsize=(4,4))
@@ -64,50 +80,3 @@ def showimlable(dataframe, n):
     print ('Post Date: ',data['post_date'])
     print ('City: ',data['city'])
     print ('Image Labels: ',data['img_lables'])
-    
-    
-def km_silhouette_visualizer(vectorized_docs, clusters_i = [12, 13, 14, 15,16,17]):
-    """
-    Displays Silhouette Visulaizer for K means clutering algorithm
-    Parameter - 
-    vectorized docs - a list doc/word vestors
-    clusters_i = list of number of clusters to compare. Accepts even number of clusters to compare. Else pads with a random number between 1-20
-    """
-    
-    from yellowbrick.cluster import SilhouetteVisualizer
-    import matplotlib.pyplot as plt
-    from sklearn.cluster import KMeans
-    import numpy as np
-    import random 
-    
-           
-    try:
-        npcluster = np.array(clusters_i).reshape(-1,2)
-    except ValueError:
-        clusters_i.append(random.randint(1,20))
-        npcluster = np.array(clusters_i).reshape(-1,2)
-        
-    r, c = np.array(npcluster).reshape(-1,2).shape
-    
-    fig, ax = plt.subplots(r, c, figsize=(15,8))
-    for i in clusters_i:
-        '''
-        Create KMeans instance for different number of clusters
-        '''
-        km = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=100, random_state=42)
-        
-        #getting positions to plot silhouette from 2Dnumpy array
-        row, column = np.where(npcluster==i)
-        modr = row[0]
-        modc = column[0]
-        
-        #q, modc = divmod(i, c)
-        #p, modr = divmod(q, r)
-        '''
-        Create SilhouetteVisualizer instance with KMeans instance
-        Fit the visualizer
-        '''
-        ax[modr, modc].set_title(f'n = {i} Clusters')
-        visualizer = SilhouetteVisualizer(km, colors='yellowbrick', ax=ax[modr][modc])
-        visualizer.fit(np.array(vectorized_docs))
-    
